07f13a22334e231d35c9e4615c3ec7f65c5bed2f
daniil <daniltihomirov946@gmail.com>
2024-11-27 15:10:30 +0300

    update

--- colorManager.js
+++ colorManager.js
@@ -1,6 +1,7 @@
 // Применяет CSS-переменные из JSON
 function applyCSSVariablesFromJSON(json) {
     for (const [key, value] of Object.entries(json)) {
+        console.log(key, value);
         document.documentElement.style.setProperty(`--${key}`, value);
     }
 }
